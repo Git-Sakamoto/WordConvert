8a005f8c1a61c4968d49d73b93b26a9e37899512
Git-Sakamoto <72164893+Git-Sakamoto@users.noreply.github.com>
2021-03-22 11:25:43 +0900

    Update input.php

--- input.php
+++ input.php
@@ -2,9 +2,12 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
 <title>入力</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 <link rel="stylesheet" type="text/css" href="stylesheet.css">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 </head>
 <body>
+    <?php include "header.html" ?>
     <div class="main">
         <div class="form">
             <form method="post" action="result.php">
