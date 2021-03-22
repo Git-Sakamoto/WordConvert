6b240ab1952afb36875d0a34c16cf21acbba02ff
Git-Sakamoto <72164893+Git-Sakamoto@users.noreply.github.com>
2021-03-22 11:16:35 +0900

    Update word.php

--- word.php
+++ word.php
@@ -9,11 +9,12 @@ $databaseManager = new DatabaseManager();
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>IT用語一覧</title>
-<link rel="stylesheet" type="text/css" href="stylesheet.css">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="stylesheet.css">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 </head>
 <body>
+    <?php include "header.html" ?>
     <div class="main">
         <div class=form>
             <img src="image/group_family_asia.png" class="left_icon">
@@ -41,7 +42,6 @@ $databaseManager = new DatabaseManager();
                 <p>データを取得できませんでした</p>
             <?php endif; ?>
             
-            <button type="button" class="button" onclick="history.back()">戻る</button>
         </div>
     </div>
 </body>
