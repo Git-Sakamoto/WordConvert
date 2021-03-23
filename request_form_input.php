262dfb6543c87156c3464a52788c3da1722b3c3a
Git-Sakamoto <72164893+Git-Sakamoto@users.noreply.github.com>
2021-03-23 10:59:55 +0900

    Update request_form_input.php

--- request_form_input.php
+++ request_form_input.php
@@ -11,12 +11,12 @@
     <div class="main">
         <div class="form">
             <form method="post" action="request_form_complete.php">
-                <img src="image/group_family_asia.png" class="left_icon">
-                <span class="form-title">単語の登録リクエスト</span>
-                <img src="image/kaden_PC.png" class="right_icon">
+                <h1>単語の登録リクエスト</h1>
+                
                 <p>・大文字を含んだアルファベット文字列、または日本語文字列は、完全一致に対応します<br>（例：IT、IoT、データベース）</p>
                 <p>・小文字だけのアルファベット文字列は、大文字小文字関係無く一致します<br>（例：user）</p>
                 <p>・最大20字</p>
+                
                 <p>単語：<input type="text" name="word[]" maxlength="20" required></p>
                 <p>単語：<input type="text" name="word[]" maxlength="20"></p>
                 <p>単語：<input type="text" name="word[]" maxlength="20"></p>
