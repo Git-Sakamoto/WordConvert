9605642a7481a784c7ec5f19a7ff4482378fe906
Git-Sakamoto <72164893+Git-Sakamoto@users.noreply.github.com>
2021-03-25 09:57:11 +0900

    Update word_edit_complete.php

--- Admin/WordList/word_edit_complete.php
+++ Admin/WordList/word_edit_complete.php
@@ -8,10 +8,13 @@ $databaseManager = new DatabaseManager();
 <html>
 <head>
     <meta charset="utf-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
     <title>編集</title>
 </head>
 <body>
-    <div class="main">
+    <?php include "../header.html" ?>
+    <div class="container">
         <h3>更新完了</h3>
         <?php
         if($databaseManager->updateTranslation($_SESSION['id'],$_POST['newBefore'],$_POST['newAfter'])) :
@@ -27,8 +30,8 @@ $databaseManager = new DatabaseManager();
                 変換前の単語：<?php echo $_POST['newBefore'] ?>が既に登録されている可能性があります
             </p>
         <?php endif; ?>
-        <button type="button" class="button" onclick="location.href='word_list.php'">単語一覧に戻る</button>
-        <button type="button" class="button" onclick="location.href='../top.php'">トップページに戻る</button>
+        <button class="btn btn-outline-primary" type="button" class="button" onclick="location.href='word_list.php'">単語一覧に戻る</button>
+        <button class="btn btn-outline-primary" type="button" class="button" onclick="location.href='../top.php'">トップページに戻る</button>
     </div>
 </body>
 </html>
