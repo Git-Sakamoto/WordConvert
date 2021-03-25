91da78de50529792f4cb129d57b319c7141847c8
Git-Sakamoto <72164893+Git-Sakamoto@users.noreply.github.com>
2021-03-25 09:51:59 +0900

    Update top.php

--- Admin/top.php
+++ Admin/top.php
@@ -4,14 +4,17 @@ require "auth.php";
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 <title>トップ</title>
 </head>
 <body>
-    <div class="main">
-        <h1>管理ページ</h1>
-        <h4><a href="NewAdd/word_input.php">新規追加</a></h4>
-        <h4><a href="WordList/word_list.php">IT用語一覧/編集/削除</a></h4>
-        <h4><a href="Request/request_list.php">リクエスト一覧</a></h4>
+    <?php include "header.html" ?>
+    <div class="container">
+        <h1>IT用語変換　管理機能</h1>
+        <h2><a href="NewAdd/word_input.php">新規登録</a></h2>
+        <h2><a href="WordList/word_list.php">IT用語一覧/編集/削除</a></h2>
+        <h2><a href="Request/request_list.php">リクエスト一覧</a></h2>
     </div>
 </body>
 </html>
